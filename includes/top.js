d21d52ae50d195f4a01d5e870823231ed9420ffd
Tom Cohen <tomcohent@gmail.com>
2015-02-14 22:10:39 +0200

    ad

--- includes/top.js
+++ includes/top.js
@@ -87,17 +87,7 @@ function createView(data){
 	       onmouseover: function(id){
 			   console.log(id.x);
 	       		d3.select('#highSalaryData').style('display','block').style('background-color','blue').
-	       		style('height','360px').style('width','350px').text("" + topInfo.get(function(error, rows) { (rows[id.x].Job
-						.toString ( function () {
-						var s = [];
-						for (var k in this) {
-							if (this.hasOwnProperty(k)) s.push(k + ':' + this[k]);
-						}
-						return '{' + s.join() + '}';
-					}
-
-
-					) )}));
+	       		style('height','360px').style('width','350px').text("" + topInfo.get(function(error, rows) { (rows[id.x].Job) }));
 	      	},
 	      	onmouseout: function(id) {
 	      		d3.select('#highSalaryData').style('display','none').text("");
