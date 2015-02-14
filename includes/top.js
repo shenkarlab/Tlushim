c6cded944514f539d749eed98b5e7116e618b165
Tom Cohen <tomcohent@gmail.com>
2015-02-14 21:16:45 +0200

    zsc

--- includes/top.js
+++ includes/top.js
@@ -86,7 +86,7 @@ function createView(data){
 	       },
 	       onmouseover: function(id){
 	       		d3.select('#highSalaryData').style('display','block').style('background-color','blue').
-	       		style('height','360px').style('width','350px').text(topInfo.get(function(error, rows) { console.log(rows[id].Data); }));
+	       		style('height','360px').style('width','350px').text(topInfo.get(function(error, rows) { console.log(rows[id].Data.toString()); }));
 	      	},
 	      	onmouseout: function(id) {
 	      		d3.select('#highSalaryData').style('display','none').text("");
