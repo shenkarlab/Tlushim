5fc97e3844e5d328224edf2fdf9f1ff52f84131c
Tom Cohen <tomcohent@gmail.com>
2015-02-14 21:09:22 +0200

    sdf

--- includes/top.js
+++ includes/top.js
@@ -73,7 +73,7 @@ function createView(data){
 		 .get(function(error, rows) { console.log(rows); });
 
 
-	console.log("this is row 0 " + topInfo.get[0]);
+	console.log("this is row 0 " + topInfo.get(function(error, rows) { console.log(rows[1]); }));
 	console.log($("svg").length);
 	if ( $("svg").length == 0){
 		chart = c3.generate({
